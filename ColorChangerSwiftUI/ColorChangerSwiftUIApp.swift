13c332a2ce9fa29466b8772316c1d67746171cc9
Anatolii-sx <s8.8@bk.ru>
2021-10-29 20:23:58 +0300

    Initial Commit

---
+++ ColorChangerSwiftUI/ColorChangerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorChangerSwiftUIApp.swift
+//  ColorChangerSwiftUI
+//
+//  Created by Анатолий Миронов on 29.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorChangerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
